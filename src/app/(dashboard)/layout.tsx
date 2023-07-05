8045bef0d6f146473d38f84e592773e63b69ac18
Pham Huu Hien <phhien203@gmail.com>
2023-07-05 09:05:16 +0700

    add hero banner

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 export default function DashboardLayout({ children }) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <div className="w-full h-[18.75rem] bg-gray-50 relative">
+        <div className="absolute inset-0 w-full h-[18.75rem] bg-gradient-to-bl from-[#5596FF] to-[#AC2DEB]"></div>
+        <div className="absolute inset-0 w-full h-[18.75rem] bg-hero-pattern object-cover bg-no-repeat bg-center opacity-25"></div>
+      </div>
+      {children}
+    </div>
+  )
 }
