721190bc0b1da383a6dbdf3b94c829a561c5fe5b
Pham Huu Hien <phhien203@gmail.com>
2023-07-07 15:02:49 +0700

    add new favicon

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function TodoHeader() {
   return (
     <header className="flex justify-between md:h-12 sm:h-5 md:mb-10 sm:mb-10">
-      <h1 className="uppercase text-white md:text-4xl sm:text-lg font-bold tracking-widest">
-        Todo
-      </h1>
+      <Link href="/">
+        <h1 className="uppercase text-white md:text-4xl sm:text-lg font-bold tracking-widest">
+          Todo
+        </h1>
+      </Link>
       <div className="self-center">
         <Image
           alt="moon icon"
