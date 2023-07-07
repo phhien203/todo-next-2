d4f708cb68fddf6a5474d3ece9fa18ea9630bacb
Pham Huu Hien <phhien203@gmail.com>
2023-07-07 07:39:49 +0700

    tweak the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1 className="text-5xl">Getting things done easily!</h1>
+      <h1 className="text-5xl uppercase text-white">
+        Getting things done easily!
+      </h1>
       <Link
         href="/todos"
-        className="inline-block mt-8 text-lg text-center px-4 py-2 bg-slate-300/50 rounded-sm"
+        className="inline-block mt-8 text-lg text-center px-4 py-2 bg-gradient-to-tr from-[#5596FF] to-[#AC2DEB] rounded-md text-white"
       >
         Get started
       </Link>
